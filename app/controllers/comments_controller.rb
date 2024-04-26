b2ae77650c5d1dc0fb86315903be70f51ca08da4
bibi-oliveira <gabrieloliveira663@icloud.com>
2024-04-26 14:18:07 -0300

    updating comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -29,6 +29,18 @@ class CommentsController < ApplicationController
       @post = Post.find(params[:post_id])
   end
 
+  def update
+    @comment = @post.comments.find(params[:id])
+    respond_to do |format|
+      if @comment.update(comment_params)
+        format.html { redirect_to post_url(@post), notice: "comment updated successfully" }
+      else
+        format.html { redirec_to post_url(@post), alert: "comment not updated" }
+      end
+    end
+
+  end
+
   def comment_params
     params.require(:comment).permit(:body)
   end
